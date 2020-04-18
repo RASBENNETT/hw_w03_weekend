8dfe340f56b5f22fbab54e4090083a1ffe6946b7
Ruairidh Benentt <ruairidhbennett@gmail.com>
2020-04-18 13:31:56 +0100

    add save and upate methods to ticket class

--- console.rb
+++ console.rb
@@ -1,46 +1,85 @@
 require('pry')
 require_relative('model/film.rb')
 require_relative('model/customer')
+require_relative('model/ticket')
 
 Film.delete_all()
 Customer.delete_all()
 
+## INITIALIZE FILMS ##
+
 film1 = Film.new( { 'title' =>  'Mary Poppins', 'price' => '10' } )
-film1.save()
-
 film2 = Film.new( { 'title' =>  'Kill Bill Vol.1', 'price' => '13' } )
-film2.save()
-
 film3 = Film.new( { 'title' =>  'Kill Bill Vol.2', 'price' => '13' } )
+film4 = Film.new( { 'title' =>  'Django Unchained', 'price' => '15' } )
+film5 = Film.new( { 'title' =>  'Star Wars III The Revenge of the Sith', 'price' => '8' } )
+film6 = Film.new( { 'title' =>  'Star Wars IV A New Hope', 'price' => '10' } )
+
+## SAVE FILMS ##
+
+film1.save()
+film2.save()
 film3.save()
-
-film3 = Film.new( { 'title' =>  'Django Unchained', 'price' => '15' } )
-film3.save()
-
-film4 = Film.new( { 'title' =>  'Star Wars III The Revenge of the Sith', 'price' => '8' } )
 film4.save()
+film5.save()
+film6.save()
 
-film4 = Film.new( { 'title' =>  'Star Wars IV A New Hope', 'price' => '10' } )
-film4.save()
+## INITIALIZE CUSTOMERS ##
+
+customer1 = Customer.new( { 'name' => 'Annie', 'funds' => '600' } )
+customer2 = Customer.new( { 'name' => 'Elvis', 'funds' => '100' } )
+
+## SAVE CUSTOMERS ##
+
+customer1.save()
+customer2.save()
+
+## INITIALIZE TICEKTS ##
+
+ticket1 = Ticket.new( { 'customer_id' => customer1.id, 'film_id' => film1.id } )
+ticket1.save()
+
+ticket2 = Ticket.new( { 'customer_id' => customer2.id, 'film_id' => film2.id } )
+ticket2.save()
+
+ticket3 = Ticket.new( { 'customer_id' => customer2.id, 'film_id' => film3.id } )
+ticket3.save()
+
+
+
+
+
+
+
+
+
+
+
+
+## UPDATES ##
+
 
 film1.price = '15'
 film1.update()
 
-
-
-customer1 = Customer.new( { 'name' => 'Annie', 'funds' => '600' } )
-customer1.save()
-
-customer2 = Customer.new( { 'name' => 'Elvis', 'funds' => '100' } )
-customer2.save()
-
 customer2.funds = '200'
 customer2.update()
 
+ticket1.film_id = film4.id
+ticket1.update()
+
+
+
+
+
+
+
+## ASSIGN VARIABLES TO LIST ALL RESULT ##
 
 films = Film.all()
 customers = Customer.all()
 
 
+## PRY ##
 binding.pry
 nil
--- model/ticket.rb
+++ model/ticket.rb
@@ -3,10 +3,29 @@ class Ticket
     attr_reader :id
     attr_accessor :customer_id, :film_id
 
-    def intitialize( options )
+    def initialize( options )
         @id = options['id'] if options['id']
         @customer_id = options['customer_id']
-        @film_id = option['film_id']
+        @film_id = options['film_id']
+    end
+
+    def save()
+        sql = "INSERT INTO tickets
+        (customer_id, film_id)
+        VALUES
+        ($1, $2)
+        RETURNING id"
+        values = [@customer_id, @film_id]
+        ticket = SqlRunner.run(sql, values).first
+        @id = ticket['id'].to_i
+    end
+
+    def update()
+        sql = "UPDATE tickets 
+        SET (customer_id, film_id) = ($1, $2)
+        WHERE id = $3"
+        values = [@customer_id, @film_id, @id]
+        SqlRunner.run(sql, values)
     end
 
 end
